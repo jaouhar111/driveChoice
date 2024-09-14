cbfd0c5ab79fa72fdd09b35989ee9cde1eb723e9
DESKTOP-V3J772C\Mehdi <elmehdijaouhar@gmail.com>
2024-09-14 16:35:07 +0100

    implement car component

--- src/app/Services/email/email-service.service.ts
+++ src/app/Services/email/email-service.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
-import { jsPDF } from 'jspdf';
 
 @Injectable({
   providedIn: 'root',
@@ -63,17 +62,12 @@ export class EmailServiceService {
     paiementData: any
   ): Promise<EmailJSResponseStatus> {
     // Generate the PDF
-    const pdfBlob = this.generatePDF(reservationId, paiementData);
-
-    // Convert the PDF Blob to Base64
-    const pdfBase64 = await this.blobToBase64(pdfBlob);
 
     // Prepare email parameters
     const templateParams = {
       client_email: clientEmail,
       client_name: clientName,
       reservation_id: reservationId,
-      pdf_attachment: pdfBase64, // Base64 string for the PDF
     };
 
     // Send the email
@@ -83,19 +77,6 @@ export class EmailServiceService {
       templateParams,
       'GIFZ9JLUsKeMJntPz' // Replace with your EmailJS public API key
     );
-  }
-
-  private generatePDF(reservationId: string, paiementData: any): Blob {
-    const doc = new jsPDF();
-
-    // Add content to the PDF
-    doc.text(`Reservation ID: ${reservationId}`, 10, 10);
-    doc.text(`Name: ${paiementData.name}`, 10, 20);
-    doc.text(`Amount: ${paiementData.amount}`, 10, 30);
-    doc.text(`Date: ${new Date().toLocaleDateString()}`, 10, 40);
-
-    // Convert PDF to Blob
-    return doc.output('blob');
   }
 
   private blobToBase64(blob: Blob): Promise<string> {
